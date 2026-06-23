57d3570833c0ff9aca237eac65dec5046d9299b3
Kauan Godoy <k1godoy07@gmail.com>
2026-06-23 14:26:46 -0300

    botao delete, adicionar e editar em funcionamento com notificacao

--- Pizzaria Godoy/Components/Pages/Category_Pages/CategoryList.razor.cs
+++ Pizzaria Godoy/Components/Pages/Category_Pages/CategoryList.razor.cs
@@ -1,5 +1,7 @@
-﻿using Pizzaria_Godoy.Data;
+﻿using Microsoft.JSInterop;
+using Pizzaria_Godoy.Data;
 using Pizzaria_Godoy.Repository;
+using Pizzaria_Godoy.Services.Extensions;
 
 namespace Pizzaria_Godoy.Components.Pages.Category_Pages
 {
@@ -7,21 +9,21 @@ namespace Pizzaria_Godoy.Components.Pages.Category_Pages
     {
         private bool IsProcessing { get; set; } = true;
         private IEnumerable<Category> Categories { get; set; } = new List<Category>();
-
+        private int DeleteCategoryID { get; set; } = 0;
         protected override async Task OnInitializedAsync()
         {
-          
+
         }
 
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
             if (firstRender)
             {
-            
+
                 await LoadCategory();
                 IsProcessing = false;
                 StateHasChanged();
-                
+
             }
             //return base.OnAfterRenderAsync(firstRender);
         }
@@ -31,6 +33,31 @@ namespace Pizzaria_Godoy.Components.Pages.Category_Pages
 
             Categories = await _categoryRepository.GetAllAsync();
             //Carregar categorias
+        }
+
+        private void HandleDelete(int id)
+        {
+
+            DeleteCategoryID = id;
+            _JS.InvokeVoidAsync("ShowConfirmationModal");
+
+        }
+        private async Task ConfirmDelete_Click(bool isConfirmed)
+        {
+            IsProcessing = true;
+            await _JS.InvokeVoidAsync("HideConfirmationModal");
+            if (isConfirmed && DeleteCategoryID != 0)
+            {
+                var result = await _categoryRepository.DeleteAsync(DeleteCategoryID);
+                if (result)
+                    _JS.ToastrSuccess("Categoria deletada");
+                else
+                    _JS.ToastrError("Erro encontrado enquanto estava deletando");
+
+                await LoadCategory();
+            }
+            DeleteCategoryID = 0;
+            IsProcessing = false;
         }
     }
 }
